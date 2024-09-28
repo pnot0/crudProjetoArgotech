faf8d42e376e764940ec79213386e32ace8ec0fe
pnot0 <39804244+pnot0@users.noreply.github.com>
2024-09-28 20:08:49 -0300

    Update ConnectionFactory.java

--- src/br/com/teste/factory/ConnectionFactory.java
+++ src/br/com/teste/factory/ConnectionFactory.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 
 public class ConnectionFactory {
 	
-	private static final String USERNAME = "root";
-	private static final String PASSWORD = "";
-	private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/teste";
+	private static final String USERNAME = "db_operator";
+	private static final String PASSWORD = "AVNS_xR3MhLBXeV_Bz0nrGir";
+	private static final String DATABASE_URL = "jdbc:mysql://argotechteste-argotech.h.aivencloud.com:14053/argotechteste";
 	
 	public static Connection createConnectionSQL() throws Exception {
 		Class.forName("com.mysql.cj.jdbc.Driver");
